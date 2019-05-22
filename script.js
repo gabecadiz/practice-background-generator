a61e81317f6509435dfb4ca43c0134bef2612564
Computer <gabecadiz@hotmail.com>
2019-05-22 17:29:42 -0400

    added random hex color script to create a random gradient background

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var body = document.getElementById('gradient');
 
+var randomButton = document.createElement('button');
+randomButton.id = 'randomButton';
+randomButton.innerText = 'Randomize!';
+body.append(randomButton);
+
 function setGradient() {
   body.style.background =
     'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
@@ -10,7 +15,24 @@ function setGradient() {
   css.textContent = body.style.background + ';';
 }
 
+function randomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function randomGradient() {
+  color1.value = randomColor();
+  color2.value = randomColor();
+  setGradient();
+}
+
 window.onload = setGradient();
+
+randomButton.addEventListener('click', randomGradient);
 
 color1.addEventListener('input', setGradient);
 
