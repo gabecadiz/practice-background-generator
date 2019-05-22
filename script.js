49778163ca923c360013d43792215cbf8e114f43
Computer <gabecadiz@hotmail.com>
2019-05-22 17:31:40 -0400

    added comments to js file

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+//elements
 var css = document.querySelector('h3');
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var body = document.getElementById('gradient');
-
 var randomButton = document.createElement('button');
+
 randomButton.id = 'randomButton';
 randomButton.innerText = 'Randomize!';
 body.append(randomButton);
 
+//functions
+//alters current gradient
 function setGradient() {
   body.style.background =
     'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
@@ -15,6 +18,7 @@ function setGradient() {
   css.textContent = body.style.background + ';';
 }
 
+//creates a random hex color
 function randomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
@@ -24,11 +28,14 @@ function randomColor() {
   return color;
 }
 
+//sets a random gradient for body background
 function randomGradient() {
   color1.value = randomColor();
   color2.value = randomColor();
   setGradient();
 }
+
+//event listeners
 
 window.onload = setGradient();
 
